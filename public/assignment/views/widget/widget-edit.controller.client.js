26e922ee7da18e655a7692f782ff80b8e14bb647
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-05-30 08:07:09 -0400

    Adding the code to select the size field and display it as per the fetched header size and minor aesthetic code changes made to the index file

--- public/assignment/views/widget/widget-edit.controller.client.js
+++ public/assignment/views/widget/widget-edit.controller.client.js
@@ -15,8 +15,44 @@
         vm.updateWidget = updateWidget;
         vm.deleteWidget = deleteWidget;
 
+        /*vm.headers = [
+            {value:'Please select', text:'Please select'},
+            {value:'1', text:'1'},
+            {value:'2', text:'2'},
+            {value:'3', text:'3'},
+            {value:'4', text:'4'},
+            {value:'5', text:'5'},
+            {value:'6', text:'6'}
+            ];*/
+        vm.headers = ['Please select', '1', '2', '3', '4', '5', '6'];
+
         function init() {
             vm.widget = WidgetService.findWidgetById(vm.widgetId);
+            if (vm.widget.size == null) {
+                vm.widget.size = vm.headers[0];
+            } else {
+                console.log("init: vm.widget.size = " + vm.widget.size);
+                switch (vm.widget.size+"") {
+                    case '1':
+                        vm.widget.size = vm.headers[1];
+                        break;
+                    case '2':
+                        vm.widget.size = vm.headers[2];
+                        break;
+                    case '3':
+                        vm.widget.size = vm.headers[3];
+                        break;
+                    case '4':
+                        vm.widget.size = vm.headers[4];
+                        break;
+                    case '5':
+                        vm.widget.size = vm.headers[5];
+                        break;
+                    case '6':
+                        vm.widget.size = vm.headers[6];
+                        break;
+                }
+            }
         }
         init();
 
