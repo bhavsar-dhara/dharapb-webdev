1cfb2eaa5fe0f2f9638df2a7839007a2402046c7
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-02 08:34:11 -0400

    Refactoring the angular service to pass the correct url for getUserByUserName method

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -27,12 +27,8 @@
         }
 
         function findUserByUsername(username) {
-            for (var i in users) {
-                if(users[i].username === username) {
-                    return users[i];
-                }
-            }
-            return null;
+            var url = "/api/user?username="+username;
+            return $http.get(url);
         }
 
         function findUserByCredentials(username, password) {
