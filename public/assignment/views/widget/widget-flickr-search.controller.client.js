4e01204807aeb2d9cbfcec31eeb080d9171deb21
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-06 16:35:22 -0400

    Adding the selectPhoto function to select the image from the Flickr Search to add on the widgetList.

--- public/assignment/views/widget/widget-flickr-search.controller.client.js
+++ public/assignment/views/widget/widget-flickr-search.controller.client.js
@@ -6,13 +6,28 @@
         .module("WebAppMaker")
         .controller("FlickrImageSearchController", FlickrImageSearchController);
     
-    function FlickrImageSearchController(FlickrService) {
+    function FlickrImageSearchController($routeParams, $location, FlickrService, WidgetService) {
         var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
 
         vm.searchPhotos = searchPhotos;
-        
+        vm.selectPhoto = selectPhoto;
+
+        function init() {
+            WidgetService
+                .findWidgetById(vm.widgetId)
+                .then(
+                    function (response) {
+                        vm.widget = response.data;
+                    }
+                );
+        }
+        init();
+
         function searchPhotos(searchText) {
-            console.log(searchText);
             FlickrService
                 .searchPhotos(searchText)
                 .then(function (response) {
@@ -21,6 +36,22 @@
                     data = JSON.parse(data);
                     vm.photos = data.photos;
                 });
+        }
+
+        function selectPhoto(photo) {
+            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
+            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
+            vm.widget.url = url;
+            WidgetService
+                .updateWidget(vm.widgetId, vm.widget)
+                .then(
+                    function (response) {
+                        $location.url("/user/"+vm.uid+"/website/"+vm.websiteId+"/page/"+vm.pageId+"/widget/"+vm.widgetId);
+                    },
+                    function (error) {
+                        vm.error = "Widget not updated";
+                    }
+                );
         }
     }
 })();
