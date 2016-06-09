a586219a3533929b8c88a3a3c26c41051e7be773
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-09 08:01:48 -0400

    Trying to add array of Website, websites attribute to the User Schema

--- assignment/models/user/user.schema.server.js
+++ assignment/models/user/user.schema.server.js
@@ -13,6 +13,8 @@ module.exports = function () {
         dob: Date,
         email: String,
         phone: String,
+        // websites: [WebsiteSchema],
+        // websites: [Schema.Types.WebsiteSchema],
         // TODO : websites of type [Website] remaining to be added
         dateCreated: {type: Date, default: Date.now}
     }, {collection: "assignment.user"});
