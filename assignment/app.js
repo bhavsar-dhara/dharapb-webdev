791efa2760d07354f81e96e29b6b18362701d4e6
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-11 19:54:10 -0400

    Adding the code to connect to mongoDB using mongoose on openshift.

--- assignment/app.js
+++ assignment/app.js
@@ -2,7 +2,23 @@
  * Created by Dhara on 5/31/2016.
  */
 module.exports = function(app) {
-    
+
+    // create a default connection string
+    var connectionString = 'mongodb://127.0.0.1:27017/cs5610summer1';
+
+    // use remote connection string
+    // if running in remote server
+    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    var mongoose = require("mongoose");
+    // connect to the database
+    mongoose.connect(connectionString);
+
     var models = require("./models/models.server.js")();
     
     require("./services/user.service.server.js")(app, models);
