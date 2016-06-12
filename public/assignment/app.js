55bf099f6c038589914a657d537a1e841cb693b9
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-11 21:24:09 -0400

    Adding the code to implement the HTML widget

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -4,5 +4,5 @@
 
 // IIFE (immediately invoked function expression)
 (function () {
-    angular.module("WebAppMaker", ["ngRoute"]);
+    angular.module("WebAppMaker", ["ngRoute", "textAngular"]);
 })();
