f7dbdb8a7c434438269d6fbaa246a66784af9d2f
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-13 11:48:22 -0400

    Committing the updated code for client side validation on login click

--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -8,21 +8,29 @@
 
     function LoginController($location, UserService) {
         var vm = this; // viewModel = vm
+        vm.isEmptyUser = false;
+        vm.isEmptyPassword = false;
+        vm.loginForm = undefined;
 
         vm.login = function(username, password) {
+            vm.isEmptyUser = vm.loginForm.username.$error.required;
+            vm.isEmptyPassword = vm.loginForm.password.$error.required;
+            console.log(vm.isEmptyUser);
             // var promise = UserService.findUserByCredentials(username, password);
             // promise.then(function (response) {
-            UserService
-                .findUserByCredentials(username, password)
-                .then(function (response) {
-                    // console.log(response);
-                    var user = response.data;
-                    if (user != null) {
-                        $location.url("/user/" + user._id);
-                    } else {
-                        vm.error = "User not found. Please register first.";
-                    }
-                });
+            if(!vm.isEmptyUser || !vm.isEmptyPassword) {
+                UserService
+                    .findUserByCredentials(username, password)
+                    .then(function (response) {
+                        // console.log(response);
+                        var user = response.data;
+                        if (user != null) {
+                            $location.url("/user/" + user._id);
+                        } else {
+                            vm.error = "User not found. Please register first.";
+                        }
+                    });
+            }
         }
     }
 })();
