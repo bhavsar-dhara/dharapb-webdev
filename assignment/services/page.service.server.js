c42ce2d2e7c119955229b8cc3bd77a491d4f754b
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-18 02:10:56 -0400

    Removing the code to set the child in the parent class in the database

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -20,69 +20,16 @@ module.exports = function (app, models) {
     function createPage(req, res) {
         var websiteId = req.params.websiteId;
         var page = req.body;
-        websiteModel
-            .findWebsiteById(websiteId)
-            .then(
-                function (website) {
-                    websiteObj = website;
-                    return pageModel
-                        .createPage(websiteId, page);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
+        pageModel
+            .createPage(websiteId, page)
             .then(
                 function (page) {
-                    websiteObj['pages'].push(page);
-                    return websiteModel
-                        .updateWebsite(websiteId, websiteObj);
-                }, function (error) {
-                    res.statusCode(400).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    return userModel
-                        .findUserById(websiteObj._user);
-                }, function (error) {
-                    res.statusCode(400).send(error);
-                }
-            )
-            .then(
-                function (user) {
-                    for (var i = 0; i < user.websites.length; i++) {
-                        if (user.websites[i] != undefined) {
-                            if (user.websites[i]._id == page._website) {
-                                delete user.websites[i];
-                                user.websites[i] = websiteObj;
-                                break;
-                            }
-                        }
-                    }
-                    userObj = user;
-                    return userModel
-                        .updateUser(websiteObj._user, userObj);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
                     res.json(page);
-                }, function (error) {
+                },
+                function (error) {
                     res.statusCode(400).send(error);
                 }
             );
-        // pageModel
-        //     .createPage(websiteId, page)
-        //     .then(
-        //         function (page) {
-        //             res.json(page);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 
     function findAllPagesForWebsite(req, res) {
@@ -116,187 +63,29 @@ module.exports = function (app, models) {
     function updatePage(req, res) {
         var pageId = req.params.pageId;
         var page = req.body;
-        websiteModel
-            .findWebsiteById(page._website)
-            .then(
-                function (website) {
-                    websiteObj = website;
-                    return pageModel
-                        .updatePage(pageId, page);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    for (var i = 0; i < websiteObj.pages.length; i++) {
-                        if (websiteObj.pages[i] != undefined) {
-                            if (websiteObj.pages[i]._id == pageId) {
-                                delete websiteObj.pages[i];
-                                websiteObj.pages[i] = page;
-                                break;
-                            }
-                        }
-                    }
-                    return websiteModel
-                        .updateWebsite(page._website, websiteObj);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    return userModel
-                        .findUserById(websiteObj._user);
-                }, function (error) {
-                    res.statusCode(400).send(error);
-                }
-            )
-            .then(
-                function (user) {
-                    for (var i = 0; i < user.websites.length; i++) {
-                        if (user.websites[i] != undefined) {
-                            if (user.websites[i]._id == page._website) {
-                                delete user.websites[i];
-                                user.websites[i] = websiteObj;
-                                break;
-                            }
-                        }
-                    }
-                    userObj = user;
-                    return userModel
-                        .updateUser(websiteObj._user, userObj);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
+        pageModel
+            .updatePage(pageId, page)
             .then(
                 function (stats) {
                     res.send(stats);
-                }, function (error) {
+                },
+                function (error) {
                     res.statusCode(400).send(error);
                 }
             );
-        // pageModel
-        //     .updatePage(pageId, page)
-        //     .then(
-        //         function (stats) {
-        //             res.send(stats);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 
     function deletePage(req, res) {
         var pageId = req.params.pageId;
         pageModel
-            .findPageById(pageId)
+            .deletePage(pageId)
             .then(
                 function (page) {
-                    pageObj = page;
-                    websiteIdObj = page._website;
-                    return websiteModel
-                        .findWebsiteById(websiteIdObj);
+                    res.json(page);
                 },
                 function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (website) {
-                    websiteObj = website;
-                    return pageModel
-                        .deletePage(pageId);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    // console.log("websiteObj before update  ==>  " + websiteObj);
-                    for (var i = 0; i < websiteObj.pages.length; i++) {
-                        if (websiteObj.pages[i] != undefined) {
-                            if (websiteObj.pages[i]._id == pageId) {
-                                delete websiteObj.pages[i];
-                                break;
-                            }
-                        }
-                    }
-                    return websiteModel
-                        .updateWebsite(pageObj._website, websiteObj);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    return userModel
-                        .findUserById(websiteObj._user);
-                }, function (error) {
-                    res.statusCode(400).send(error);
-                }
-            )
-            .then(
-                function (user) {
-                    for (var i = 0; i < user.websites.length; i++) {
-                        if (user.websites[i] != undefined) {
-                            // TODO comparison not working as expected
-                            if (user.websites[i]._id == websiteIdObj) {
-                                delete user.websites[i];
-                                user.websites[i] = websiteObj;
-                                break;
-                            } else {
-                                console.log("NOT EQUAL");
-                            }
-                        }
-                    }
-                    userObj = user;
-                    return userModel
-                        .updateUser(websiteObj._user, userObj);
-                    // function (user) {
-                    //     console.log("websiteObj after update  ==>  " + websiteObj);
-                    //     for (var i = 0; i < user.websites.length; i++) {
-                    //         if (user.websites[i] != undefined) {
-                    //             console.log(typeof user.websites[i]._id);
-                    //             console.log(user.websites[i]._id);
-                    //             console.log(typeof websiteIdObj);
-                    //             console.log(pageObj._website);
-                    //             if (user.websites[i]._id == pageObj._website) {
-                    //                 // console.log("user.websites[i] before update  ==>  " + user.websites[i]);
-                    //                 delete user.websites[i];
-                    //                 user.websites[i] = websiteObj;
-                    //                 // console.log("user.websites[i] after update  ==>  " + user.websites[i]);
-                    //                 break;
-                    //             } else {
-                    //                 console.log(pageObj._website);
-                    //             }
-                    //         }
-                    //     }
-                    //     userObj = user;
-                    //     return userModel
-                    //         .updateUser(websiteObj._user, userObj);
-                }, function (error) {
-                    res.statusCode(404).send(error);
-                }
-            )
-            .then(
-                function (stats) {
-                    res.send(stats);
-                }, function (error) {
                     res.statusCode(400).send(error);
                 }
             );
-        // pageModel
-        //     .deletePage(pageId)
-        //     .then(
-        //         function (page) {
-        //             res.json(page);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 };
--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -15,42 +15,16 @@ module.exports = function (app, models) {
     function createWebsite(req, res) {
         var userId = req.params.userId;
         var website = req.body;
-        userModel
-            .findUserById(userId)
+        websiteModel
+            .createWebsite(userId, website)
             .then(
-                function (user) {
-                    websiteModel
-                        .createWebsite(userId, website)
-                        .then(
-                            function (website) {
-                                user['websites'].push(website);
-                                userModel
-                                    .updateUser(userId, user)
-                                    .then(
-                                        function (stats) {
-                                            res.json(website);
-                                        }, function (error) {
-                                            res.statusCode(400).send(error);
-                                        }
-                                    );
-                            }, function (error) {
-                                res.statusCode(400).send(error);
-                            }
-                        );
-                }, function (error) {
-                    res.statusCode(404).send(error);
+                function (website) {
+                    res.json(website);
+                },
+                function (error) {
+                    res.statusCode(400).send(error);
                 }
             );
-        // websiteModel
-        //     .createWebsite(userId, website)
-        //     .then(
-        //         function (website) {
-        //             res.json(website);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 
     function findAllWebsitesForUser(req, res) {
@@ -84,105 +58,29 @@ module.exports = function (app, models) {
     function updateWebsite(req, res) {
         var websiteId = req.params.websiteId;
         var website = req.body;
-        userModel
-            .findUserById(website._user)
+        websiteModel
+            .updateWebsite(websiteId, website)
             .then(
-                function (user) {
-                    websiteModel
-                        .updateWebsite(websiteId, website)
-                        .then(
-                            function (stats) {
-                                for (var i = 0; i < user.websites.length; i++) {
-                                    if (user.websites[i] != undefined) {
-                                        if (user.websites[i]._id == websiteId) {
-                                            delete user.websites[i];
-                                            user.websites[i] = website;
-                                            break;
-                                        }
-                                    }
-                                }
-                                userModel
-                                    .updateUser(website._user, user)
-                                    .then(
-                                        function (stats) {
-                                            res.send(stats);
-                                        }, function (error) {
-                                            res.statusCode(400).send(error);
-                                        }
-                                    );
-                            }, function (error) {
-                                res.statusCode(404).send(error);
-                            }
-                        );
-                }, function (error) {
-                    res.statusCode(404).send(error);
+                function (stats) {
+                    res.send(stats);
+                },
+                function (error) {
+                    res.statusCode(400).send(error);
                 }
             );
-        // websiteModel
-        //     .updateWebsite(websiteId, website)
-        //     .then(
-        //         function (stats) {
-        //             res.send(stats);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 
     function deleteWebsite(req, res) {
         var websiteId = req.params.websiteId;
         websiteModel
-            .findWebsiteById(websiteId)
+            .deleteWebsite(websiteId)
             .then(
                 function (website) {
-                    userModel
-                        .findUserById(website._user)
-                        .then(
-                            function (user) {
-                                websiteModel
-                                    .deleteWebsite(websiteId)
-                                    .then(
-                                        function (stats) {
-                                            for (var i = 0; i < user.websites.length; i++) {
-                                                if (user.websites[i] != undefined) {
-                                                    if (user.websites[i]._id == websiteId) {
-                                                        delete user.websites[i];
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                            userModel
-                                                .updateUser(website._user, user)
-                                                .then(
-                                                    function (stats) {
-                                                        res.send(stats);
-                                                    }, function (error) {
-                                                        res.statusCode(400).send(error);
-                                                    }
-                                                );
-                                        }, function (error) {
-                                            res.statusCode(404).send(error);
-                                        }
-                                    );
-                            }, function (error) {
-                                res.statusCode(404).send(error);
-                            }
-                        );
+                    res.json(website);
                 },
                 function (error) {
-                    res.statusCode(404).send(error);
+                    res.statusCode(400).send(error);
                 }
             );
-        // websiteModel
-        //     .deleteWebsite(websiteId)
-        //     .then(
-        //         function (website) {
-        //             res.json(website);
-        //         },
-        //         function (error) {
-        //             res.statusCode(400).send(error);
-        //         }
-        //     );
     }
 };
