a24299a11f6f742e7b6e56d40922919c8840c398
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-23 11:28:38 -0400

    Committing the code to implement query changes and toggling of the eventList on the screen-size change

--- public/project/services/eventful.service.client.js
+++ public/project/services/eventful.service.client.js
@@ -10,8 +10,8 @@
     // http://api.eventful.com/json/events/search?app_key=TjLhBLhJm4SCTsMx&keywords=books&location=San+Diego&date=Future
     // var key = "Pc55hFFcncSQH6qW";
     var key = "TjLhBLhJm4SCTsMx";
-    var urlBaseInit = "http://api.eventful.com/json/events/search?app_key=API_KEY&category=music&location=LOCATION&date=Future&sort_order=date&sort_direction=ascending&callback=JSON_CALLBACK";
-    var urlBase = "http://api.eventful.com/json/events/search?app_key=API_KEY&category=music&keywords=TEXT&location=LOCATION&date=Future&sort_order=date&sort_direction=ascending&callback=JSON_CALLBACK";
+    var urlBaseInit = "http://api.eventful.com/json/events/search?app_key=API_KEY&category=music&location=LOCATION&date=This Week&sort_order=popularity&sort_direction=ascending&callback=JSON_CALLBACK";
+    var urlBase = "http://api.eventful.com/json/events/search?app_key=API_KEY&category=music&keywords=TEXT&location=LOCATION&date=This Week&sort_order=popularity&sort_direction=ascending&callback=JSON_CALLBACK";
 
     function EventfulService($http) {
         var api = {
@@ -22,7 +22,7 @@
         function searchEvents(searchText, location) {
             var url = "";
             if(location == null) {
-                location = Boston;
+                location = "Boston";
             }
             if(searchText == null) {
                 url = urlBaseInit
