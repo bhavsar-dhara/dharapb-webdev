85f8118a1cbe5e73ebc945d4414e961683aaa9e6
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-06-26 21:34:12 -0400

    Committing the code to remove the markers before setting the new ones.

--- public/project/views/event/event.controller.client.js
+++ public/project/views/event/event.controller.client.js
@@ -33,7 +33,7 @@
                         console.log(response);
 
                         map = new google.maps.Map(document.getElementById('map'), {
-                            center: {lat: 38.9072, lng: -77.0369},
+                            center: {lat: 42.3601, lng: -71.0589},
                             zoom: 10,
                             disableDefaultUI: true
                         });
@@ -185,6 +185,11 @@
                         data = response.data;
                         // console.log("data = " + JSON.stringify(data));
                         vm.events = data.events;
+
+                        for (var j=0; j < vm.markers.length; j++) {
+                            vm.markers[j].setMap(null);
+                        }
+                        vm.markers = [];
 
                         // console.log(vm.events.event.length);
                         for (var i = 0; i < vm.events.event.length; i++){
